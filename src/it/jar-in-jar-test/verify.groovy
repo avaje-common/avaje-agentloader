33e1c6767704fa310f7c70bf2ff3f02437cfd35f
Roland Praml <roland.praml@foconis.de>
2019-02-10 16:17:20 +0100

    ADD: IT-Tests

---
+++ src/it/jar-in-jar-test/verify.groovy
@@ -0,0 +1,40 @@
+import java.io.*;
+
+File file1 = new File( basedir, "target/avaje-agentloader-jar-in-jar-test-" + projectVersion + "-classname.jar" );
+File file2 = new File( basedir, "target/avaje-agentloader-jar-in-jar-test-" + projectVersion + "-prefix.jar" );
+
+assert file1.isFile();
+assert file2.isFile();
+
+// check the "uber-jar" file size
+assert file1.length() < 50000000;
+assert file2.length() < 50000000;
+
+
+// 
+jar1:{
+  def command = "java -jar " + file1;
+  def process = command.execute();
+  process.waitFor();
+
+  def output = process.in.text
+  def err = process.err.text
+  // and check if the enhancer has kicked in
+  assert err.equals("")
+  assert output.contains("public java.lang.Object misc.domain.Entity2._ebean_newInstance()")
+  assert output.contains("searching for io.ebean.enhance.Transformer")
+}
+
+jar2:{
+  // launch the second jar
+  def command = "java -jar " + file2;
+  def process = command.execute();
+  process.waitFor();
+
+  def output = process.in.text
+  def err = process.err.text
+  // and check if the enhancer has kicked in
+  assert err.equals("")
+  assert output.contains("public java.lang.Object misc.domain.Entity2._ebean_newInstance()")
+  assert output.contains("searching for ebean-agent")
+}
